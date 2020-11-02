26939591646704ad748e353323f04e75143ce228
Ulises Rosas <urosas@sahuarolabs.com>
2020-11-01 21:47:05 -0700

    base client hooks

---
+++ public/extensions/custom/hooks/clientes/hooks.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'actions' => [
+        'item.create.cliente:after' => function($data) {
+            //TODO email feature
+        },
+        'item.update.cliente:after' => function($data) {
+            //TODO admin email feature
+        }
+    ]
+];
