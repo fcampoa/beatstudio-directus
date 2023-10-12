967859b3401e03b74f12f2ef698ebb629eff7ee1
fcampoa <felixcampoa@gmail.com>
2023-10-12 13:30:16 -0700

    endpoint

--- public/extensions/custom/endpoints/historial_compra/endpoints.php
+++ public/extensions/custom/endpoints/historial_compra/endpoints.php
@@ -489,5 +489,36 @@ return [
                 ]);
             }
         }
+    ],
+    '/cumpleanos-mes' => [
+        'method' => 'GET',
+        'handler' => function(Request $request, Response $response) {
+            $servername = "localhost";
+        $username = "root";
+        $password = "B34tsp1n";
+        $database = "beatstudio-test";
+        
+        $conn = new mysqli($servername, $username, $password, $database);
+        if($conn->connect_error) {
+            die("connection failed: " . $conn->connect_error);
+        }
+        $month = date('n');
+        $sql = "SELECT nombre, apellido, fecha_nacimiento FROM cliente WHERE MONTH(fecha_nacimiento) = " . $month;
+        $result = $conn->query($sql);
+        $response = array();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+		$response[] = $row;
+            }
+             // echo "nombre: " . $row["nombre"]. " - apellido: " . $row["apellido"]. " " . $row["fecha_nacimiento"]. "<br>";
+             header('Content-Type: application/json');
+             return $response->withJson(['data' => $response]);
+        }
+        else {
+            return$response->withJson([ 'data' => "0 results"]);
+          }
+          $conn->close();
+          exit;
+        }
     ]
 ];
