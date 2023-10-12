913e0691d8b05c54c50d25ae4e144a4423ab6f1f
fcampoa <felixcampoa@gmail.com>
2023-10-12 15:00:34 -0700

    options en headers

--- src/functions/cumpleanos-mes.php
+++ src/functions/cumpleanos-mes.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {    // The request is using the POST method
